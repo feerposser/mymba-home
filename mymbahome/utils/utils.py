bcad2672915ea9fedf8a5d085ba74e347318c232
Fernando Posser Pinheiro <fernando.posser@hotmail.com>
2021-04-07 08:55:37 -0300

    'get_total_impacted_animals' from 'utils' handle exception. 'See more actions' buttom from 'activities.html' handle when rendered in home and '/atividades/'

--- mymbahome/utils/utils.py
+++ mymbahome/utils/utils.py
@@ -4,8 +4,8 @@ from projects.models import ModelActivity
 def get_total_impacted_animals():
     try:
         count = 0
-        for activity in ModelActivity.objects.all():
+        for activity in ModelActivity.objects.all().exclude(type="deleted"):
             count += activity.impacted_animals
         return count
-    except Exception as e:
-        print(e)
+    except:
+        return 0
